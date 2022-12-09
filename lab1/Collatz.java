a20b0868f3503e75bdb53949744af4ec6034b7c9
MeteorVMeteor <wwl1284431903@gmail.com>
2022-12-09 21:57:46 +0900

    right one add

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,6 +5,7 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
+	// right one
         if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
